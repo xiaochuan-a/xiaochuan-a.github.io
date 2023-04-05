97289687d718dbb52637b50ff15ec0a8364781e9
丁航川 <d3182818374@163.com>
2023-04-05 17:54:17 +0800

    Site updated: 2023-04-05 17:54:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/sdas2d.html","posts/dgdsf.html","posts/sdasdd.html","posts/undefined.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/sdas2d.html","posts/sdasdd.html","posts/undefined.html","posts/dgdsf.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
